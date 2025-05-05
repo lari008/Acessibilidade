5d52b2d5fc0c1d2561fef2a22d091600f49c4f1a
Larissa Manuela <146357365+lari008@users.noreply.github.com>
2025-05-05 08:50:39 -0300

    botaoDeAcessibilidade

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded'), function(){
-   const botaoDeAcessibilidade = document.getElementById
+   const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+   const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+   botaoDeAcessibilidade.addEventListener('click',function(){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+   })
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
